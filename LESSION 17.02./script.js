24f49aea14363ac9972eb2b8f9bbd2b1662738c4
klintat <klintat@github.com>
2022-02-17 21:23:58 +0200

    update

--- LESSION 17.02./script.js
+++ LESSION 17.02./script.js
@@ -1,5 +1,5 @@
 // let myObjekct = {
-  
+
 //   anotherfunction: function() {
 //       console.log("Added form")
 //   },
@@ -32,4 +32,50 @@
 let d = new Date();
 
 this.alert("Today is " + d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear());
-this.alert('Today is ${d.getDate()}
+// this.alert("Today is ${d.getDate()}.${(d.getMonth() + 1}.${d.getFullYear()}");
+
+let age = parseInt(this.prompt("How old are you?"));
+// // if (age > 17)//same as below
+// if (age >= 18) {
+//     this.alert("You can buy a glass of wine");
+// } else {
+//     this.alert("You are not allowed to buy the alcohol");
+// }
+
+
+// if (!(age < 18)) {
+//     this.alert("You are not allowed to buy the alchohol");
+// } else {
+//     this.alert("You can buy glass of wine")
+// }
+
+// if (!(age < 18))
+//     this.alert("You can buy a glass of wine")
+// this.alert("One two three");
+
+
+// if (age < 18)
+//     this.alert("You are underage");
+// else if (age >= 18 && age < 21)
+//     this.alert("You can't buy the alko in US");
+// else if (age < 21)
+//     this.alert("Your age is under 21"); <-- This will never be executed
+// else
+//     this.alert("Enjoy your wine");
+
+
+if (age < 18)
+    this.alert("You are underage");
+else {
+    if (age >= 18 && age < 21)
+    if (!(!(age >= 18 || age < 21)))
+        this.alert("You can't buy the alko in US");
+    else
+        this.alert("Enjoy your wine");
+}
+
+//De Morgan's law 
+/// !(cond1 || cond2) same as !cond1 && ! cond2
+/// !(cond1 && cond2) same as !cond1 || !cond2 
+
+
