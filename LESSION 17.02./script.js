844da51444b09605f585f43ee645915272e7a9cc
klintat <klintat@github.com>
2022-02-17 20:23:20 +0200

    update

---
+++ LESSION 17.02./script.js
@@ -0,0 +1,35 @@
+// let myObjekct = {
+  
+//   anotherfunction: function() {
+//       console.log("Added form")
+//   },
+//     myFunction: function() {
+//         this.anotherfunction()
+//   }
+//  }
+
+// myObject.myFunction();
+
+// alert("Here is the alert");
+// this.alert("Here is the alert")
+
+// let answer = this.confirm("Do you want to continue?")
+// console.log(answer);
+
+// answer = this.prompt("How are you?", "Good!");
+// console.log(answer);
+// this.open("https://" + answer);
+
+
+// answer = this.prompt("Which page you want to visit?",). replace("http://", "").replace("https://", "")
+// console.log(answer);
+// this.open("https://" + answer);
+
+
+// answer = this.prompt("Which page you want to visit?", "http://");
+// this.open(answer);
+
+let d = new Date();
+
+this.alert("Today is " + d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear());
+this.alert('Today is ${d.getDate()}
