914bb1b8c037c5334d33d422209f2881d003ff30
klintat <klintat@github.com>
2022-03-11 19:01:39 +0200

    calculator task

--- LESSION_07.03._calculator/calculator.js
+++ LESSION_07.03._calculator/calculator.js
@@ -2,25 +2,41 @@ var firstNumber, hasFirstNumber, sign;
 
 function numberPress (event) {
     let number = $(event.currentTarget).val();
-    let originaVal = $("#textArea").val();
-    $("#textArea").val(originaVal + number);
+    let originalVal = $("#textArea").val();
+    if (!((originalVal.indexOf(".") !== -1 || originalVal.length === 0) && number === ".") &&
+        !(originalVal === "0" && number === "0") &&
+        originalVal.length < 10) {
+        $("#textArea").val(originalVal + number);
+    }
 }
 
 function clearPress (event) {
     $("#textArea").val("");
+    this.firstNumber = undefined;
+    this.hasFirstNumber = undefined;
+    this.sign = undefined;
+    $(".operator").prop("disabled", false);
 }
 
 function processPress (event) {
-    this.firstNumber = $("#textArea").val();
-    $("#textArea").val("");
-    this.hasFirstNumber = true;
-    this.sign = $(event.currentTarget).val();
+    if ($("#textArea").val() !== "") {
+        this.firstNumber = $("#textArea").val();
+        $("#textArea").val("");
+        this.hasFirstNumber = true;
+        this.sign = $(event.currentTarget).val();
+        $(".operator").prop("disabled", true);
+    }
 }
 
 function resultPress (event) {
     if (this.firstNumber) {
-        this.firstNumber = parseInt(this.firstNumber, 10);
-        let secondNumber = parseInt($("#textArea").val(), 10);
+        this.firstNumber = parseFloat(this.firstNumber, 10);
+        let secondNumber;
+        if ($("#textArea").val() === "") {
+            secondNumber = 0;
+        } else {
+            secondNumber = parseFloat($("#textArea").val(), 10);
+        }
         let calculatedValue;
         switch (this.sign) {
             case "+":
@@ -36,6 +52,46 @@ function resultPress (event) {
                 calculatedValue = this.firstNumber / secondNumber;
                 break;
         }
-        $("#textArea").val(calculatedValue);
+        $("#textArea").val(Math.round(calculatedValue * 100000000) / 100000000);
+        $(".operator").prop("disabled", false);
+    }
+}
+
+function plusMinus (event) {
+    let originalVal = $("#textArea").val();
+    if (["", "0", "0."].indexOf(originalVal) === -1) {
+        let newVal;
+        if (originalVal[0] === "-") {
+            newVal = originalVal.substring(1);
+        } else {
+            newVal = "-" + originalVal;
+        }
+        $("#textArea").val(newVal);
+    }
+}
+
+function squareRoot (event) {
+    let originalVal = parseFloat($("#textArea").val(), 10);
+    if (originalVal >= 0) {
+        let root = Math.sqrt(originalVal);
+        $("#textArea").val(Math.round(root * 100000000) / 100000000);
+    }
+}
+
+function del (event) {
+    let originalVal = $("#textArea").val();
+    if (originalVal !== "") {
+        $("#textArea").val(originalVal.substring(0, originalVal.length - 1));
+        if ($("#textArea").val() === "-") {
+            $("#textArea").val("");
+        }
+    }
+}
+
+function precent (event) {
+    let originalVal = $("#textArea").val();
+    if (originalVal !== "") {
+        let result = parseFloat(originalVal, 10) / 100;
+        $("#textArea").val(Math.round(result * 100000000) / 100000000);
     }
 }
